1cce3a1338a3284c985f57d4ef90d4fc78f18aa4
Sulaiman Shamasna <sulaiman.shamasna@gmail.com>
2024-06-16 00:51:55 +0200

    feat: :sparkles: Add feature to check shape of the data

---
+++ neural_machine_translation/check_shape.py
@@ -0,0 +1,39 @@
+import numpy as np
+
+import typing
+from typing import Any, Tuple
+
+import einops
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
+
+import tensorflow as tf
+import tensorflow_text as tf_text
+
+
+class ShapeChecker():
+  def __init__(self):
+    # Keep a cache of every axis-name seen
+    self.shapes = {}
+
+  def __call__(self, tensor, names, broadcast=False):
+    if not tf.executing_eagerly():
+      return
+
+    parsed = einops.parse_shape(tensor, names)
+
+    for name, new_dim in parsed.items():
+      old_dim = self.shapes.get(name, None)
+      
+      if (broadcast and new_dim == 1):
+        continue
+
+      if old_dim is None:
+        # If the axis name is new, add its length to the cache.
+        self.shapes[name] = new_dim
+        continue
+
+      if new_dim != old_dim:
+        raise ValueError(f"Shape mismatch for dimension: '{name}'\n"
+                         f"    found: {new_dim}\n"
+                         f"    expected: {old_dim}\n")
