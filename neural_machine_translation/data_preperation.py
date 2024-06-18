8e91cdcfef692fdf92ef842aff8c5f1b9b63d5ed
Sulaiman Shamasna <sulaiman.shamasna@gmail.com>
2024-06-18 17:08:25 +0200

    chore: :fire: this file will be removed soon

--- neural_machine_translation/data_preperation.py
+++ neural_machine_translation/data_preperation.py
@@ -3,166 +3,37 @@ import tensorflow_text as tf_text
 import tensorflow as tf
 import pathlib
 from typing import Tuple
+import matplotlib.pyplot as plt
+import matplotlib.ticker as ticker
 
-path_to_zip = tf.keras.utils.get_file(
-    'spa-eng.zip', origin='http://storage.googleapis.com/download.tensorflow.org/data/spa-eng.zip',
-    extract=True
-)
+from check_shape import ShapeChecker
+from preprocessing import *
+from models import *
+from train import *
 
-path_to_file = pathlib.Path(path_to_zip).parent / 'spa-eng/spa.txt'
 
-def load_data(path: pathlib.Path) -> Tuple[np.ndarray, np.ndarray]:
-    """
-    Loads and processes the translation data from a given file path.
-    
-    Args:
-        path (pathlib.Path): The path to the file containing the translation pairs.
+import textwrap
 
-    Returns:
-        Tuple[np.ndarray, np.ndarray]: Two numpy arrays, one for the target language sentences and one for the context language sentences.
-    """
-    text = path.read_text(encoding='utf-8')
-    lines = text.splitlines()
-    pairs = [line.split('\t') for line in lines]
+print('Expected output:\n', '\n'.join(textwrap.wrap(target_raw[-1])))
 
-    context = np.array([context for target, context in pairs])
-    target = np.array([target for target, context in pairs])
+# # model.plot_attention(long_text)
 
-    return target, context
+inputs = [
+    'Hace mucho frio aqui.', # "It's really cold here."
+    'Esta es mi vida.', # "This is my life."
+    'Su cuarto es un desastre.' # "His room is a mess"
+]
 
-target_raw, context_raw = load_data(path_to_file)
+# %%time
+for t in inputs:
+    print(model.translate([t])[0].numpy().decode())
 
-# Create tf.data dataset
-
-BUFFER_SIZE = len(context_raw)
-BATCH_SIZE = 64
-
-is_train = np.random.uniform(size=(len(target_raw),)) < 0.8
-
-train_raw = (
-    tf.data.Dataset
-    .from_tensor_slices((context_raw[is_train], target_raw[is_train]))
-    .shuffle(BUFFER_SIZE)
-    .batch(BATCH_SIZE)
-)
-val_raw = (
-    tf.data.Dataset
-    .from_tensor_slices((context_raw[~is_train], target_raw[~is_train]))
-    .shuffle(BUFFER_SIZE)
-    .batch(BATCH_SIZE)
-)
-
-'''
-for example_context_strings, example_target_strings in train_raw.take(1):
-    print(example_context_strings[:10])
     print()
-    print(example_target_strings[:10])
-    break
-'''
 
-example_text = tf.constant('¿Todavía está en casa?')
+    # %%time
+    result = model.translate(inputs)
 
-print(example_text.numpy())
-print(tf_text.normalize_utf8(example_text, 'NFKD').numpy())
-
-
-def tf_lower_and_split_punct(text):
-  # Split accented characters.
-  text = tf_text.normalize_utf8(text, 'NFKD')
-  text = tf.strings.lower(text)
-  # Keep space, a to z, and select punctuation.
-  text = tf.strings.regex_replace(text, '[^ a-z.?!,¿]', '')
-  # Add spaces around punctuation.
-  text = tf.strings.regex_replace(text, '[.?!,¿]', r' \0 ')
-  # Strip whitespace.
-  text = tf.strings.strip(text)
-
-  text = tf.strings.join(['[START]', text, '[END]'], separator=' ')
-  return text
-
-print(example_text.numpy().decode())
-print(tf_lower_and_split_punct(example_text).numpy().decode())
-
-
-# Text Vectorization
-
-max_vocab_size = 5000
-
-context_text_processor = tf.keras.layers.TextVectorization(
-    standardize=tf_lower_and_split_punct,
-    max_tokens=max_vocab_size,
-    ragged=True)
-     
-context_text_processor.adapt(train_raw.map(lambda context, target: context))
-
-# Here are the first 10 words from the vocabulary:
-context_text_processor.get_vocabulary()[:10]
-
-
-target_text_processor = tf.keras.layers.TextVectorization(
-    standardize=tf_lower_and_split_punct,
-    max_tokens=max_vocab_size,
-    ragged=True)
-
-target_text_processor.adapt(train_raw.map(lambda context, target: target))
-target_text_processor.get_vocabulary()[:10]
-
-
-example_tokens = context_text_processor(example_context_strings)
-example_tokens[:3, :]
-
-context_vocab = np.array(context_text_processor.get_vocabulary())
-tokens = context_vocab[example_tokens[0].numpy()]
-' '.join(tokens)
-
-
-plt.subplot(1, 2, 1)
-plt.pcolormesh(example_tokens.to_tensor())
-plt.title('Token IDs')
-
-plt.subplot(1, 2, 2)
-plt.pcolormesh(example_tokens.to_tensor() != 0)
-plt.title('Mask')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    print(result[0].numpy().decode())
+    print(result[1].numpy().decode())
+    print(result[2].numpy().decode())
+    print()
