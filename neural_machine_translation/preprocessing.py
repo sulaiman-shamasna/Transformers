c31c6dc2588d3e10ed21a3e733fb01ed807e184a
Sulaiman Shamasna <sulaiman.shamasna@gmail.com>
2024-06-18 17:09:23 +0200

    refactor: :recycle: Refactor the preprocessing steps

--- neural_machine_translation/preprocessing.py
+++ neural_machine_translation/preprocessing.py
@@ -6,30 +6,18 @@ from typing import Tuple, Callable
 import matplotlib.pyplot as plt
 import matplotlib.ticker as ticker
 
-def download_and_extract_data(url: str, extract: bool = True) -> pathlib.Path:
-    """
-    Downloads and extracts the dataset from the given URL.
-    
-    Args:
-        url (str): URL to the dataset.
-        extract (bool): Whether to extract the dataset after downloading.
-    
-    Returns:
-        pathlib.Path: Path to the extracted dataset file.
-    """
+def download_and_extract_data(
+                            url: str,
+                            extract: bool = True
+                            ) -> pathlib.Path:
+
     path_to_zip = tf.keras.utils.get_file('spa-eng.zip', origin=url, extract=extract)
     return pathlib.Path(path_to_zip).parent / 'spa-eng/spa.txt'
 
-def load_data(path: pathlib.Path) -> Tuple[np.ndarray, np.ndarray]:
-    """
-    Loads and processes the translation data from a given file path.
-    
-    Args:
-        path (pathlib.Path): The path to the file containing the translation pairs.
+def load_data(
+            path: pathlib.Path
+            ) -> Tuple[np.ndarray, np.ndarray]:
 
-    Returns:
-        Tuple[np.ndarray, np.ndarray]: Two numpy arrays, one for the target language sentences and one for the context language sentences.
-    """
     text = path.read_text(encoding='utf-8')
     lines = text.splitlines()
     pairs = [line.split('\t') for line in lines]
@@ -39,16 +27,10 @@ def load_data(path: pathlib.Path) -> Tuple[np.ndarray, np.ndarray]:
 
     return target, context
 
-def tf_lower_and_split_punct(text: tf.Tensor) -> tf.Tensor:
-    """
-    Text standardization function to lower case and split punctuation.
-    
-    Args:
-        text (tf.Tensor): Input text tensor.
-    
-    Returns:
-        tf.Tensor: Processed text tensor.
-    """
+def tf_lower_and_split_punct(
+                        text: tf.Tensor
+                        ) -> tf.Tensor:
+
     text = tf_text.normalize_utf8(text, 'NFKD')
     text = tf.strings.lower(text)
     text = tf.strings.regex_replace(text, '[^ a-z.?!,¿]', '')
@@ -57,18 +39,13 @@ def tf_lower_and_split_punct(text: tf.Tensor) -> tf.Tensor:
     text = tf.strings.join(['[START]', text, '[END]'], separator=' ')
     return text
 
-def create_text_processor(vocab_size: int, dataset: tf.data.Dataset, map_func: Callable[[tf.Tensor, tf.Tensor], tf.Tensor]) -> tf.keras.layers.TextVectorization:
-    """
-    Creates and adapts a TextVectorization layer.
-    
-    Args:
-        vocab_size (int): Maximum vocabulary size.
-        dataset (tf.data.Dataset): Dataset to adapt the processor on.
-        map_func (Callable[[tf.Tensor, tf.Tensor], tf.Tensor]): Function to map the dataset elements for the processor.
-    
-    Returns:
-        tf.keras.layers.TextVectorization: Adapted TextVectorization layer.
-    """
+def create_text_processor(
+                        vocab_size: int,
+                        dataset: tf.data.Dataset,
+                        map_func: Callable[[tf.Tensor, tf.Tensor],
+                        tf.Tensor]
+                        ) -> tf.keras.layers.TextVectorization:
+
     text_processor = tf.keras.layers.TextVectorization(
         standardize=tf_lower_and_split_punct,
         max_tokens=vocab_size,
@@ -76,37 +53,25 @@ def create_text_processor(vocab_size: int, dataset: tf.data.Dataset, map_func: C
     text_processor.adapt(dataset.map(map_func))
     return text_processor
 
-def process_text(context: tf.Tensor, target: tf.Tensor, context_processor: tf.keras.layers.TextVectorization, target_processor: tf.keras.layers.TextVectorization) -> Tuple[Tuple[tf.Tensor, tf.Tensor], tf.Tensor]:
-    """
-    Processes the text for input to the model.
-    
-    Args:
-        context (tf.Tensor): Context sentences.
-        target (tf.Tensor): Target sentences.
-        context_processor (tf.keras.layers.TextVectorization): Processor for context sentences.
-        target_processor (tf.keras.layers.TextVectorization): Processor for target sentences.
-    
-    Returns:
-        Tuple[Tuple[tf.Tensor, tf.Tensor], tf.Tensor]: Processed context and target tensors.
-    """
+def process_text(
+                context: tf.Tensor,
+                target: tf.Tensor,
+                context_processor: tf.keras.layers.TextVectorization,
+                target_processor: tf.keras.layers.TextVectorization
+                ) -> Tuple[Tuple[tf.Tensor, tf.Tensor], tf.Tensor]:
+
     context = context_processor(context).to_tensor()
     target = target_processor(target)
     targ_in = target[:,:-1].to_tensor()
     targ_out = target[:,1:].to_tensor()
     return (context, targ_in), targ_out
 
-def prepare_datasets(target: np.ndarray, context: np.ndarray, batch_size: int) -> Tuple[tf.data.Dataset, tf.data.Dataset]:
-    """
-    Prepares the training and validation datasets.
-    
-    Args:
-        target (np.ndarray): Array of target sentences.
-        context (np.ndarray): Array of context sentences.
-        batch_size (int): Batch size for the datasets.
-    
-    Returns:
-        Tuple[tf.data.Dataset, tf.data.Dataset]: Training and validation datasets.
-    """
+def prepare_datasets(
+                    target: np.ndarray,
+                    context: np.ndarray,
+                    batch_size: int
+                    ) -> Tuple[tf.data.Dataset, tf.data.Dataset]:
+
     buffer_size = len(context)
     is_train = np.random.uniform(size=(len(target),)) < 0.8
 
@@ -126,13 +91,7 @@ def prepare_datasets(target: np.ndarray, context: np.ndarray, batch_size: int) -
     return train_raw, val_raw
 
 def main() -> Tuple[tf.data.Dataset, tf.data.Dataset, tf.keras.layers.TextVectorization, tf.keras.layers.TextVectorization, np.ndarray, np.ndarray]:
-    """
-    Main function to prepare datasets and text processors.
 
-    Returns:
-        Tuple[tf.data.Dataset, tf.data.Dataset, tf.keras.layers.TextVectorization, tf.keras.layers.TextVectorization, np.ndarray, np.ndarray]:
-        Training dataset, validation dataset, context text processor, target text processor, target raw data, context raw data.
-    """
     path_to_file = download_and_extract_data('http://storage.googleapis.com/download.tensorflow.org/data/spa-eng.zip')
     target_raw, context_raw = load_data(path_to_file)
 
